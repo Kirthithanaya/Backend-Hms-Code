ccaacb040af66314c65a153e3f3c3f3ab5e6228b
Kirthithanaya <kkeerthika546@gmail.com>
2025-05-22 08:29:13 +0530

    Added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt"
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -45,17 +45,6 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-// Hash password before save
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
 
-// Match password
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 export default mongoose.model("User", userSchema);
